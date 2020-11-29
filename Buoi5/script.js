7a1d4281d17f281f843373895c235a5cf1e55ccf
kyphamvan <ky01642748487@gmail.com>
2020-11-29 08:18:09 +0700

    29/11 add folder Buoi5

---
+++ Buoi5/script.js
@@ -0,0 +1,18 @@
+//tong
+sum = (a,b) => {
+    let sumAB = a+b;
+    console.log(sumAB);
+}
+
+sum(100,200);
+//hien thi index 7, xoa 5,length
+arr = () =>{
+var arrName = ["1","2","3","4","5","6","7","8","9","10"];
+console.log(arrName.length);
+console.log(arrName[7]);
+arrName.splice(5,1);
+console.log(arrName[3]);
+console.log(arrName[4]);
+console.log(arrName[5]);
+}
+arr();
